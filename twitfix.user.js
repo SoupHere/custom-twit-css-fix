8d42dbe115140da38254198a3b5a7249aae031ca
SoupHere <k47spartan@hotmail.com>
2022-01-29 00:23:26 -0500

    Update twitfix.user.js
    More asterisks always good

--- twitfix.user.js
+++ twitfix.user.js
@@ -3,7 +3,7 @@
 // @namespace https://github.com/SoupHere/custom-twit-css-fix
 // @description fixes twitter's terrible blur filter BS
 // Only run on this domain:
-// @match https://twitter.com/*
+// @include *twitter.com/*
 // ==/UserScript==
 var cssTxt = ".r-yfv4eo {" +
     "filter: blur(0px) !important;" +
