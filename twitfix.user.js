87ce4db0cff0d994a26e24e8f673a70afc28d1db
SoupHere <k47spartan@hotmail.com>
2022-01-29 00:12:51 -0500

    Update twitfix.user.js
    s

--- twitfix.user.js
+++ twitfix.user.js
@@ -3,7 +3,7 @@
 // @namespace https://github.com/SoupHere/custom-twit-css-fix
 // @description fixes twitter's terrible blur filter BS
 // Only run on this domain:
-// @match http://twitter.com/*
+// @match https://twitter.com/*
 // ==/UserScript==
 var cssTxt = ".r-yfv4eo {" +
     "filter: blur(0px) !important;" +
