2958bc14c53d09ce32bb261d3a2179acf507427d
SoupHere <k47spartan@hotmail.com>
2022-01-28 23:42:01 -0500

    Update twitfix.user.js
    hardcoded because I hate undocumented functions

--- twitfix.user.js
+++ twitfix.user.js
@@ -3,10 +3,13 @@
 // @namespace https://github.com/SoupHere/custom-twit-css-fix
 // @description fixes twitter's terrible blur filter BS
 // Only run on this domain:
-// @include http://twitter.com/*
-// @resource customCSS https://github.com/SoupHere/custom-twit-css-fix/raw/main/twitfix.css
+// @match http://twitter.com/*
 // ==/UserScript==
-console.debug('start: add CSS');
-var cssTxt  = GM_getResourceText("customCSS");
-GM_addStyle (cssTxt);
-console.debug('done: add CSS');
+var cssTxt = ".r-yfv4eo {" +
+    "filter: blur(0px) !important;" +
+    "}";
+(function () {
+    var s = document.createElement('style');
+    s.innerHTML = cssTxt;
+    (document.head || document.documentElement).appendChild(s);
+})();
